b087c586eedcd347bb2e9f35c8d2464142dbf33f
hutamy <hutamytriesthi@gmail.com>
2021-03-16 22:26:10 +0700

    add tailwind color config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,5 @@
 const defaultTheme = require('tailwindcss/defaultTheme')
+const colors = require('tailwindcss/colors')
 
 module.exports = {
   purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
@@ -8,6 +9,20 @@ module.exports = {
       fontFamily: {
         sans: ['Inter var', ...defaultTheme.fontFamily.sans],
       },
+      colors: {
+        transparent: 'transparent',
+        current: 'currentColor',
+        black: colors.black,
+        white: colors.white,
+        gray: colors.trueGray,
+        indigo: colors.indigo,
+        red: colors.rose,
+        yellow: colors.amber,
+        pink: colors.pink,
+        green: colors.green,
+        blue: colors.blue,
+        purple: colors.purple
+      }
     },
   },
   variants: {
